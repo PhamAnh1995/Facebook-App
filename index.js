433deebe6ae5261f9ce3a94de446ef7925d78fb3
PhamAnh1995 <PhamAnh1995@gmail.com>
2021-10-13 01:00:41 +0700

    commit source

---
+++ index.js
@@ -0,0 +1,11 @@
+// var openBTn = document.getElementById('openBtn')
+// var popup = document.getElementById('popup')
+// var closeBtn = document.getElementById('closeBtn')
+
+// openBTn.addEventListener('click',()=>{
+//     popup.style.display='inline'
+// })
+
+// closeBtn.addEventListener('click',()=>{
+//     popup.style.display='none'
+// })
